acfb9d15c24f064a2db84b69a02b558ecb4dd5ba
ReturnReason <reasonz@daum.net>
2019-04-28 14:33:40 +0900

    제거

--- leeyou.c
+++ leeyou.c
@@ -6,19 +6,9 @@
 /*
 git hub 연습해보기
 */
-void print_you()
-{
-
-	printf("이유\n");
-	printf("git 충돌 확인 연습\n");
-	printf("\n");
-
-}
 
 int main(void) 
 {
-
-	print_you();
 
 		char name[3][16];
 		int korean[3], english[3], math[3];
